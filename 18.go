5be5f806483e5244a2a41d1f386c3d1f5947e303
mazhichao <mazhichao@192.168.0.101>
2022-04-24 21:33:44 +0800

    18.go

--- 18.go
+++ 18.go
@@ -1,5 +1,14 @@
 package main
 
-func main() {
+import "fmt"
 
+func fourSum(nums []int, target int) [][]int {
+	ans := make([][]int, 0)
+
+	return ans
+}
+
+func main() {
+	ans := fourSum([]int{2, 2, 2, 2}, 8)
+	fmt.Println(ans)
 }
